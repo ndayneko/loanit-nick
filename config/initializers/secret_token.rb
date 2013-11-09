b9696e70e81bbefba3e8572bfa471288339508a9
mega <nickday@ubuntu.ubuntu-domain>
2013-11-09 15:57:32 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Loanittome::Application.config.secret_key_base = '7a1770f8e6e612c8c3cefabb328770b313bd9e12853cd6cd055151fc47e9c84cf5a996bf7537b7621c7e8305082176e81e4697060fd567c1edf98ed2ac547feb'
